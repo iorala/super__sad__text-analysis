538651841d9df502a76e3c6507b303b27b8563d5
iorala <andreas.laroi@gmail.com>
2023-04-26 15:42:15 +0200

    Template_Struktur für Flask Web-Applikation hinzugefügt

---
+++ main.py
@@ -0,0 +1,47 @@
+# benötigte Module importieren
+from flask import Flask, redirect
+from flask import render_template
+from flask import request
+from collections import defaultdict
+
+# name der Applikation
+app = Flask("super().__sad__(text_analysis)")
+
+# Globale Variablen und Funktionen
+variable = "Inhalt"
+
+
+# @app.name_der_Funktion()
+#
+#def funktion():
+#   return "x"
+
+#
+# Webseiten
+#
+
+# Homepage
+@app.route('/')
+def home():
+    return render_template("main.html")
+
+# andere Seite
+@app.route('/name_seite', methods=["GET", "POST"])
+# - Streckenübersicht
+# - Filtermöglichkeiten für Suche (Ersetzt separate Suchseite)
+def show_routes():
+    # Blah blah
+    return render_template("name_seite.html")
+
+# Test-Seite -> gibt einfach Variablen aus
+@app.route('/test')
+# For testing
+def hello_world(variable):
+
+    return variable
+
+#
+# App Ausführen
+#
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
