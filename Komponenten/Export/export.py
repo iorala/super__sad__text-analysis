2c4c4762d657d1b1f6c3a7b8a7c060b3ce47757f
Nadwayne <131520543+Nadwayne@users.noreply.github.com>
2023-05-20 14:47:37 +0200

    Update export.py

--- Komponenten/Export/export.py
+++ Komponenten/Export/export.py
@@ -1,3 +1,5 @@
+import Visualization
+
 class Export:
     def __init__(self, name: str, data: bytes):
         self.name = name
@@ -5,8 +7,15 @@ class Export:
 
     def compress_data(self) -> bytes:
         # Implementiere die Komprimierung des Datenstroms
-        pass
+        compressed_data = Visualization.compress(self.data)
+        return compressed_data
 
     def save_file(self) -> bool:
-        # Implementiere das Speichern der Datei
-        pass
+       # Implementiere das Speichern der Datei
+        try:
+            with open(self.name, "wb") as file:
+                file.write(self.data)
+            return True
+        except Exception as e:
+            print(f"Fehler beim Speichern der Datei: {str(e)}")
+            return False
