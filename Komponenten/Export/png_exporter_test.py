e1576aa905b0cb6a5f935654c69ae5c49b628c08
Nadwayne <131520543+Nadwayne@users.noreply.github.com>
2023-05-20 12:18:07 +0200

    Create png_exporter_test.py

---
+++ Komponenten/Export/png_exporter_test.py
@@ -0,0 +1,10 @@
+import unittest
+from png_exporter import PNGExporter
+
+class PNGExporterTest(unittest.TestCase):
+    def test_png_exporter(self):
+        # Test für den PNGExporter
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
