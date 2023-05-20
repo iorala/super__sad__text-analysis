bdebc04f4505ef0995806b6b1a0a343a140e768c
Nadwayne <131520543+Nadwayne@users.noreply.github.com>
2023-05-20 12:16:37 +0200

    Create export_test.py

---
+++ Komponenten/Export/export_test.py
@@ -0,0 +1,14 @@
+import unittest
+from export import Exporter
+
+class ExporterTest(unittest.TestCase):
+    def test_compress_data(self):
+        # Test für die Komprimierung der Daten
+        pass
+
+    def test_save_file(self):
+        # Test für das Speichern der Datei
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
