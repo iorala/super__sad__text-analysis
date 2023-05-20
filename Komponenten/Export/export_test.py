cd0a83e92fb034de732fc666cadc517e6c047859
Nadwayne <131520543+Nadwayne@users.noreply.github.com>
2023-05-20 15:12:45 +0200

    Update export_test.py

--- Komponenten/Export/export_test.py
+++ Komponenten/Export/export_test.py
@@ -2,20 +2,12 @@ import unittest
 from export import Export
 import Visualization
 
-
 class ExportTest(unittest.TestCase):
-    def test_compress_data(self):
-        # Test für die Komprimierung der Daten
-        export = Export("test", b"test data")
-        compressed_data = export.compress_data()
-        self.assertEqual(compressed_data, b"compressed data")  # Beispiel: Überprüfung der Komprimierung
-
     def test_save_file(self):
         # Test für das Speichern der Datei
         export = Export("test", b"test data")
         success = export.save_file()
         self.assertTrue(success)  # Beispiel: Überprüfung des Speicherns
-
 
 if __name__ == '__main__':
     unittest.main()
