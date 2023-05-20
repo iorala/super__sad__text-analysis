4274bedc93aea450f8de5afc3fdf8ac994683290
Nadwayne <131520543+Nadwayne@users.noreply.github.com>
2023-05-20 12:17:33 +0200

    Create image_data_test.py

---
+++ Komponenten/Export/image_data_test.py
@@ -0,0 +1,10 @@
+import unittest
+from image_data import ImageData
+
+class ImageDataTest(unittest.TestCase):
+    def test_image_properties(self):
+        # Test für die Eigenschaften der Bild-Daten
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
