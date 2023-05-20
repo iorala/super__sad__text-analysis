b63e8ca2f32ec84221f0fedd0e940a8cd8abee11
Nadwayne <131520543+Nadwayne@users.noreply.github.com>
2023-05-20 14:56:24 +0200

    Update png_exporter_test.py

--- Komponenten/Export/png_exporter_test.py
+++ Komponenten/Export/png_exporter_test.py
@@ -1,10 +1,15 @@
 import unittest
 from png_exporter import PNGExporter
 
+
 class PNGExporterTest(unittest.TestCase):
     def test_png_exporter(self):
         # Test für den PNGExporter
-        pass
+        exporter = PNGExporter("image.png", b"image data", 90)
+        self.assertEqual(exporter.name, "image.png")
+        self.assertEqual(exporter.data, b"image data")
+        self.assertEqual(exporter.quality, 90)
+
 
 if __name__ == '__main__':
     unittest.main()
