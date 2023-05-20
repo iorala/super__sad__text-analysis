43c513b6de5f22912efa75bb85e28c08de3e39b3
Nadwayne <131520543+Nadwayne@users.noreply.github.com>
2023-05-20 14:51:15 +0200

    Update image_data_test.py

--- Komponenten/Export/image_data_test.py
+++ Komponenten/Export/image_data_test.py
@@ -1,10 +1,14 @@
 import unittest
 from image_data import ImageData
 
+
 class ImageDataTest(unittest.TestCase):
     def test_image_properties(self):
         # Test für die Eigenschaften der Bild-Daten
-        pass
+        image_data = ImageData(b"image data", 640, 480)
+        self.assertEqual(image_data.height, 640)
+        self.assertEqual(image_data.width, 480)
+
 
 if __name__ == '__main__':
     unittest.main()
