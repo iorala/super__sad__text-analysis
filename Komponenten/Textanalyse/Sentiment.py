02572ce0cb322187c5a6a35a5de8a664ed11644e
iorala <andreas.laroi@gmail.com>
2023-05-26 21:54:01 +0200

    Bugfix Sentiment.py: Umgang mit leeren Zeilen im CSV

--- Komponenten/Textanalyse/Sentiment.py
+++ Komponenten/Textanalyse/Sentiment.py
@@ -7,11 +7,11 @@ class Sentiments:
         self.rows = rows
 
     def analyze_sentiment(self, text):
-    # mit Hilfe der Bibliothek BlobDE (für Deutsche Texte) werden die einzelnen Texte klassifiziert dafür wird aus dem Text ein TextBlob Objekt generiert
+        # mit Hilfe der Bibliothek BlobDE (für Deutsche Texte) werden die einzelnen Texte klassifiziert dafür wird aus dem Text ein TextBlob Objekt generiert
         text_blob = TextBlobDE(text)
-    #mit Polarity wird ein Float zwischen -1 und 1 zurückgeben, wobei -1 negativ, 1 positiv und 0 neutral ist
+        # mit Polarity wird ein Float zwischen -1 und 1 zurückgeben, wobei -1 negativ, 1 positiv und 0 neutral ist
         polarity = text_blob.sentiment.polarity
-    #Die Float Werte sollen direkt in den drei Klassen Positiv, Negativ und Neutral ausgeben werden
+        # Die Float Werte sollen direkt in den drei Klassen Positiv, Negativ und Neutral ausgeben werden
         if polarity > 0:
             return 'Positiv'
         elif polarity < 0:
@@ -19,15 +19,18 @@ class Sentiments:
         else:
             return 'Neutral'
 
-    #Jeder Zeile wird eine Klassifikation mittels analyse_sentiment zugewiesen. Das Resultat wir in einer Liste gespeichert.
+    # Jeder Zeile wird eine Klassifikation mittels analyse_sentiment zugewiesen. Das Resultat wir in einer Liste gespeichert.
     def get_sentiments(self):
         sentiments = []
         for row in self.rows:
-            sentiment = self.analyze_sentiment(row[0])
-            sentiments.append(sentiment)
+            # Andreas: der Code stürzt sonst ab, wenn die Zeile leer ist
+            if row:
+                sentiment = self.analyze_sentiment(row[0])
+                sentiments.append(sentiment)
         return sentiments
 
-#-------------------------------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------------------------------------
 
 
 class SentimentDataFrame:
@@ -35,9 +38,11 @@ class SentimentDataFrame:
         self.rows = rows
         self.sentiments_df = None
 
-    #Die importierten Daten werden mit den Klassifikationen aus der Sentiment-Analyse in ein DataFrame gespeichert
+    # Die importierten Daten werden mit den Klassifikationen aus der Sentiment-Analyse in ein DataFrame gespeichert
     def create_dataframe(self, sentiments):
-        self.sentiments_df = pd.DataFrame({"Text": [row[0] for row in self.rows], "Sentiment": sentiments})
+        # self.sentiments_df = pd.DataFrame({"Text": [row[0] for row in self.rows], "Sentiment": sentiments})
+        # Andreas: Auch hier funktioniert es sonst nicht, wenn die Zeile leer ist
+        self.sentiments_df = pd.DataFrame({"Text": [row[0] for row in self.rows if row], "Sentiment": sentiments})
 
     def get_dataframe(self):
         return self.sentiments_df
