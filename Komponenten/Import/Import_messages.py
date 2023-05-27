151a58bd35ac0da966bfb7e899fc2739726f8e19
iorala <andreas.laroi@gmail.com>
2023-05-27 15:44:09 +0200

    ANpaasungen Fehlermeldungen

--- Komponenten/Import/Import_messages.py
+++
@@ -1,6 +0,0 @@
-import_messages= {
-    0: "Datenimport erfolgreich.",
-    2: "Fehler: File konnte nicht gefunden werden oder falscher Dateipfad."
-    3: "Fehler: Mehrere Spalten in der Datei vorhanden. Es ist nur eine Spalte mit Inputs erlaubt."
-    4: "Fehler: Nicht-zeichenketten Werte in der CSV-Datei gefunden."
-}
---
+++ Messages.py
@@ -0,0 +1,7 @@
+import_messages= {
+    0: "Prozess erfolreich abgeschlossen",
+    1: " nicht abgeschlossen",
+    2: "Fehler: File konnte nicht gefunden werden oder falscher Dateipfad.",
+    3: "Fehler: Mehrere Spalten in der Datei vorhanden. Es ist nur eine Spalte mit Inputs erlaubt.",
+    4: "Fehler: Nicht-zeichenketten Werte in der CSV-Datei gefunden."
+}
