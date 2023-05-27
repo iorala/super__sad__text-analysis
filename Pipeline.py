7b80d91c46d134664ca252e5a6a008ec87a9aa87
Nadwayne <131520543+Nadwayne@users.noreply.github.com>
2023-05-27 16:29:58 +0200

    Create Pipeline.py

---
+++ Pipeline.py
@@ -0,0 +1,13 @@
+
+# Komponenten laden
+
+# Datenimport
+from Komponenten.Import.Import_and_Control import DataImport, DataControl
+
+# Textanalyse
+from Komponenten.Textanalyse.Sentiment import Sentiments, SentimentResult
+
+# Visualisierung
+from Komponenten.Visualisierung.DataVisualiser import DataVisualiser
+
+    
