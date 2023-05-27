1ba00d420c59907a83861acac8523fae0b2b1d25
iorala <andreas.laroi@gmail.com>
2023-05-27 15:45:03 +0200

    ANpaasungen Fehlermeldungen

--- Messages.py
+++ Messages.py
@@ -1,5 +1,5 @@
 import_messages= {
-    0: "Prozess erfolreich abgeschlossen",
+    0: "Prozess erfolgreich abgeschlossen",
     1: " nicht abgeschlossen",
     2: "Fehler: File konnte nicht gefunden werden oder falscher Dateipfad.",
     3: "Fehler: Mehrere Spalten in der Datei vorhanden. Es ist nur eine Spalte mit Inputs erlaubt.",
