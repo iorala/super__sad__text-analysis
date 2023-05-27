2c378c30879a12e1f525869110ca0e3894697708
iorala <andreas.laroi@gmail.com>
2023-05-27 18:21:31 +0200

    Separates Analyseskript erstellen

---
+++ pipeline_analysis.py
@@ -0,0 +1,11 @@
+from Komponenten.Import.Import_and_Control import DataImport, DataControl, Import_Constants
+from Komponenten.Textanalyse.Sentiment import Sentiments, SentimentResult
+from Komponenten.Visualisierung.DataVisualiser_1 import DataVisualiser
+
+
+def main(datei):
+    print("Starte Pipeline")
+
+
+if __name__ == "__main__":
+    main("test.csv")
