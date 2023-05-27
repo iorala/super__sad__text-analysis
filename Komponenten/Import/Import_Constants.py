c8378b6f802f64975553e138bf2d0feeccefab15
iorala <andreas.laroi@gmail.com>
2023-05-27 14:37:03 +0200

    Errormeldungen Import integrieren

---
+++ Komponenten/Import/Import_Constants.py
@@ -0,0 +1,8 @@
+class Import_Constants():
+    SUCCESS = 0
+    NOT_STARTED = 1
+    FILE_NOT_FOUND = 2
+
+
+
+print(Import_error_msg[Import_Constants.SUCCESS])
---
+++ Komponenten/Import/Import_messages.py
@@ -0,0 +1,4 @@
+import_messages= {
+    0: "Datenimport erfolgreich.",
+    2: "Fehler: File konnte nicht gefunden werden oder falscher Dateipfad."
+}
