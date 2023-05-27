bcf3773cc144d7222d1ca61a1d564169e8c74e94
iorala <andreas.laroi@gmail.com>
2023-05-27 15:46:59 +0200

    Messages in Klasse umwandeln

--- Messages.py
+++ Messages.py
@@ -1,7 +1,11 @@
-import_messages= {
-    0: "Prozess erfolgreich abgeschlossen",
-    1: " nicht abgeschlossen",
-    2: "Fehler: File konnte nicht gefunden werden oder falscher Dateipfad.",
-    3: "Fehler: Mehrere Spalten in der Datei vorhanden. Es ist nur eine Spalte mit Inputs erlaubt.",
-    4: "Fehler: Nicht-zeichenketten Werte in der CSV-Datei gefunden."
-}
+class Messages:
+    def __init__(self):
+        self.messages = {
+            0: "Prozess erfolgreich abgeschlossen",
+            1: " nicht abgeschlossen",
+            2: "Fehler: File konnte nicht gefunden werden oder falscher Dateipfad.",
+            3: "Fehler: Mehrere Spalten in der Datei vorhanden. Es ist nur eine Spalte mit Inputs erlaubt.",
+            4: "Fehler: Nicht-zeichenketten Werte in der CSV-Datei gefunden."
+        }
+    def get_message(self, code):
+        return self.messages[code]
