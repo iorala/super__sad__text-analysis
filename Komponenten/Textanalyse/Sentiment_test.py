b629fe0bcdbd605fa8c2345159f27d6705a175d9
lydia-sa <131520604+lydia-sa@users.noreply.github.com>
2023-06-02 16:01:30 +0200

    new test Sentiment_test.py

--- Komponenten/Textanalyse/Sentiment_test.py
+++ Komponenten/Textanalyse/Sentiment_test.py
@@ -3,30 +3,46 @@ import pandas as pd
 from Sentiment import SentimentResult
 from Sentiment import SentimentAnalyse
 
-class SentimentResultTest(unittest.TestCase):
-    def setUp(self):
-        self.rows = [('Es ist ein wunderschöner Tag.',), ('Das Leben ist scheisse.',)]
-        self.sentiments = ['Positiv', 'Negativ']
-        self.sentiment_dataframe = SentimentResult(self.rows)
-
-    def test_create_dataframe(self):
-        self.sentiment_dataframe.create_dataframe(self.sentiments)
-        data_dict = self.sentiment_dataframe.result_dict()
-        expected_dataframe = pd.DataFrame({'Sentiment': ['Es ist ein wunderschöner Tag.', 'Das Leben ist scheisse.'],
-                                           'Count': ['Positiv', 'Negativ']})
-        self.assertEqual(data_dict.equals(expected_dataframe), True)
-
-
-class SentimentsTest(unittest.TestCase):
-    def setUp(self):
-        self.rows = [('Es ist ein wunderschöner Tag.',), ('Das Buch ist schlecht.',)]
-        self.sentiments = SentimentAnalyse(self.rows)
-
+class TestSentimentAnalyse(unittest.TestCase):
     def test_get_sentiments(self):
-        expected_sentiments = ["Positiv", "Negativ"]
-        sentiments = self.sentiments.get_sentiments()
-        self.assertEqual(sentiments, expected_sentiments)
+        # Test data
+        rows = [
+            ['Es ist ein wunderschöner Tag.'],
+            ['Der Service ist nicht gut.'],
+            ['Es ist okay.'],
+            ['Das Buch ist schlecht.'],
+            ['Ich liebe es.']
+        ]
+        analyser = SentimentAnalyse()
+        analyser.set_rows(rows)
+        result = analyser.get_sentiments()
 
+        #Resultat überprüfen
+        self.assertEqual(result.positiv_count, 2)
+        self.assertEqual(result.negativ_count, 2)
+        self.assertEqual(result.neutral_count, 1)
+
+if __name__ == '__main__':
+    unittest.main()
+
+class TestSentimentResult(unittest.TestCase):
+    def test_get_result_as_dict(self):
+        result = SentimentResult()
+        # Test data
+        result.positiv_count = 2
+        result.negativ_count = 3
+        result.neutral_count = 1
+
+        # Get the result as a dictionary
+        sentiment_counts = result.get_result_as_dict()
+
+        # Verify the results
+        expected_counts = {
+            'Positiv': 2,
+            'Negativ': 3,
+            'Neutral': 1
+        }
+        self.assertEqual(sentiment_counts, expected_counts)
 
 if __name__ == '__main__':
     unittest.main()
