a4004d2709a21d0868a55932cf28db9632d768df
Nadwayne <131520543+Nadwayne@users.noreply.github.com>
2023-06-08 15:20:07 +0200

    Delete Komponenten/Export directory

--- Komponenten/Export/Image_data.py
+++
@@ -1,5 +0,0 @@
-class ImageData:
-    def __init__(self, data: bytes, height: int, width: int):
-        self.data = data
-        self.height = height
-        self.width = width
--- Komponenten/Export/PNG_Exporter.py
+++
@@ -1,7 +0,0 @@
-from Komponenten.Export.Export import Export
-
-
-class PNGExporter(Export):
-    def __init__(self, name: str, data: bytes, quality: int):
-        super().__init__(name, data)
-        self.quality = quality
