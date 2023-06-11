a52a0b326ad8ea52d84f7f0f8339391ffd8f8e97
guemah1 <m.guener@hotmail.ch>
2023-06-11 17:52:19 +0200

    Funktionen im Code kommentiert.

--- Tests/Textanalyse/Sentiment_test.py
+++ Tests/Textanalyse/Sentiment_test.py
@@ -4,6 +4,9 @@ from Komponenten.Textanalyse.Sentiment import SentimentAnalyse
 
 
 class TestSentimentAnalyse(unittest.TestCase):
+    # Testet die Funktion get_sentiments der SentimentAnalyse-Klasse, um die Sentiment-Analyse für eine Liste von
+    # Textzeilen durchzuführen. Überprüft die Anzahl der positiven, negativen und neutralen Sentiments und verwendet
+    # assertEqual, um die erwarteten Ergebnisse zu überprüfen.
     def test_get_sentiments(self):
         # Test data
         rows = [
@@ -28,6 +31,9 @@ if __name__ == '__main__':
 
 
 class TestSentimentResult(unittest.TestCase):
+    # Testet die Funktion get_result_as_dict der SentimentResult-Klasse, um das Ergebnis der Sentiment-Analyse als
+    # Wörterbuch zurückzugeben. Setzt zuvor die Werte für die positiven, negativen und neutralen Zählungen. Überprüft,
+    # ob das Wörterbuch den erwarteten Zählungen entspricht, und verwendet assertEqual für die Überprüfung.
     def test_get_result_as_dict(self):
         result = SentimentResult()
         # Test data
