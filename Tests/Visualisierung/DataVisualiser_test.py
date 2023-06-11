ddc409d14459c6bcc5596683423a1bd96ff0f314
guemah1 <m.guener@hotmail.ch>
2023-06-11 17:56:37 +0200

    Funktionen im Code kommentiert.

--- Tests/Visualisierung/DataVisualiser_test.py
+++ Tests/Visualisierung/DataVisualiser_test.py
@@ -5,16 +5,22 @@ sentiments_dict = {'Positiv': 2, 'Negativ': 1, 'Neutral': 3}
 
 visualizer = DataVisualiser(sentiments_dict)
 
+# Zeigt das Balkendiagramm an.
 fig_1 = visualizer.plot_bar_chart()
+# Zeigt das Kreisdiagramm an.
 fig = visualizer.plot_pie_chart()
+
 table = visualizer.display_table()
 
 fig_1.show()
 fig.show()
+# Gibt die Tabelle mit den Sentiment-Daten aus.
 print(table)
 
 # Diagramme speichern
 bar_chart_file, pie_chart_file = visualizer.save_visualizations("sentiment_analysis")
 
+# Gibt den Dateinamen des gespeicherten Balkendiagramms aus.
 print("Bar Chart wurde gespeichert unter:", bar_chart_file)
+# Gibt den Dateinamen des gespeicherten Kreisdiagramms aus.
 print("Pie Chart wurde gespeichert unter:", pie_chart_file)
