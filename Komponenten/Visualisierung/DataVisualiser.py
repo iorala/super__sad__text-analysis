aa5318aa17038b640355e41b38f5fc0c43e1e7f9
guemah1 <m.guener@hotmail.ch>
2023-06-11 17:36:01 +0200

    Funktionen im Code kommentiert.

--- Komponenten/Visualisierung/DataVisualiser.py
+++ Komponenten/Visualisierung/DataVisualiser.py
@@ -6,6 +6,7 @@ class DataVisualiser:
     def __init__(self, data):
         self.data = data
 
+    # Erstellt ein Balkendiagramm (Bar Chart) basierend auf den Daten und gibt das Plotly-Figure-Objekt zurück.
     def plot_bar_chart(self):
         fig_1 = px.bar(x=list(self.data.keys()), y=list(self.data.values()), color=self.data.keys(),
                        color_discrete_map={'Neutral': 'lightgrey', 'Negativ': 'indianred', 'Positiv': 'forestgreen'})
@@ -19,6 +20,7 @@ class DataVisualiser:
         fig_1.update_yaxes(showgrid=True, gridwidth=0.5, gridcolor='#878787')
         return fig_1
 
+    # Erstellt ein Kreisdiagramm (Pie Chart) basierend auf den Daten und gibt das Plotly-Figure-Objekt zurück.
     def plot_pie_chart(self):
         fig = px.pie(values=list(self.data.values()), names=list(self.data.keys()), color=self.data.keys(),
                      color_discrete_map={'Neutral': 'lightgrey', 'Negativ': 'indianred', 'Positiv': 'forestgreen'})
@@ -27,10 +29,13 @@ class DataVisualiser:
         )
         return fig
 
+    # Erstellt eine Pandas DataFrame-Tabelle basierend auf den Daten und gibt sie zurück.
     def display_table(self):
         table = pd.DataFrame(list(self.data.items()), columns=["Sentiment", "Count"])
         return table
 
+    #  Speichert die generierten Visualisierungen (Bar Chart und Pie Chart)
+    #  als Bilddateien ab und gibt die Dateinamen zurück.
     def save_visualizations(self, file_prefix):
         bar_chart_file = file_prefix + "_bar_chart.png"
         pie_chart_file = file_prefix + "_pie_chart.png"
@@ -61,24 +66,29 @@ class VisualisationHandler:
         self.visualizer = DataVisualiser(self.data_dict)
         self.result = VisResult()
 
+    # Führt die Handhabung von beiden Visualisierungen (Pie Chart und Bar Chart) durch.
     def handle_all(self):
         self.handle_pie()
         self.handle_bar()
 
+    # Führt die Handhabung des Pie Charts durch.
     def handle_pie(self):
         if self.result.pie is None:
             self.result.pie = self.visualizer.plot_pie_chart()
 
+    # Führt die Handhabung des Bar Charts durch.
     def handle_bar(self):
         if self.result.bar is None:
             self.result.bar = self.visualizer.plot_bar_chart()
 
+    # Speichert das Pie Chart als Bilddatei ab und gibt den Dateinamen zurück.
     def save_pie(self, file_prefix):
         pie_chart_file = file_prefix + "_pie_chart.png"
         # Pie Chart speichern
         self.result.pie.write_image(pie_chart_file)
         return pie_chart_file
 
+    # Speichert das Bar Chart als Bilddatei ab und gibt den Dateinamen zurück.
     def save_bar(self, file_prefix):
         bar_chart_file = file_prefix + "_pie_chart.png"
         # Pie Chart speichern
